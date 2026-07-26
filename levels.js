07b79e74e7b7192deee2e69ff696ab0190990e35
kapitanmelanzu <144041692+kapitanmelanzu@users.noreply.github.com>
2026-07-26 23:25:08 +0200

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,51 @@
+const levels = [
+
+{
+    name:"Poziom 0",
+    objective:"Stwórz Tatę Tomka",
+    start:"T",
+    target:"TT",
+    letters:["T"]
+},
+
+{
+    name:"Poziom 1",
+    objective:"Znajdź Tomka",
+    start:"TT",
+    target:"STT",
+    letters:["S","T"]
+},
+
+{
+    name:"Poziom 2",
+    objective:"Znajdź Wujka Tomka",
+    start:"TT",
+    target:"BTT",
+    letters:["B","T"]
+},
+
+{
+    name:"Poziom 3",
+    objective:"Znajdź Siostrę Tomka",
+    start:"STT",
+    target:"CTSTT",
+    letters:["C","T"]
+},
+
+{
+    name:"Poziom 4",
+    objective:"Znajdź Dziadka Tomka",
+    start:"TT",
+    target:"TTT",
+    letters:["T","B","S"]
+},
+
+{
+    name:"Poziom 5",
+    objective:"Znajdź Żonę Tomka",
+    start:"BTT",
+    target:"ZSBBTT",
+    letters:["Z","B","S"]
+}
+
+];
